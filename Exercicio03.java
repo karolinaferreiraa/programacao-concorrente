85a233d90782eb7c4244c9501e79840baa5c6e1c
karolinaferreiraa <karolinaferreira518@gmail.com>
2026-03-11 21:47:21 -0300

    Exericios3

---
+++ Exercicio03.java
@@ -0,0 +1,89 @@
+import java.util.Random;
+
+
+public class Exercicio03 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        short[] numeros = new short[1000000000];
+        Random random = new Random();
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (short) random.nextInt(1, Short.MAX_VALUE);
+        }
+
+        long inicio = System.currentTimeMillis();
+
+        long soma = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            soma += numeros[i];
+        }
+
+        long tempo = System.currentTimeMillis() - inicio;
+
+        System.out.println("\nsoma sequencial: " + soma);
+        System.out.println("tempo: " + tempo + " ms");
+
+        inicio = System.currentTimeMillis();
+
+        int numThreads = 10;
+        SomaThread[] threads = new SomaThread[numThreads];
+        int faixa = numeros.length / numThreads;
+
+        int inicioFaixa = 0;
+
+        for (int i = 0; i < numThreads; i++) {
+
+            int fimFaixa = inicioFaixa + faixa;
+
+            threads[i] = new SomaThread(numeros, inicioFaixa, fimFaixa);
+            threads[i].setName("Thread-" + i);
+            threads[i].start();
+
+            inicioFaixa = fimFaixa;
+        }
+
+        soma = 0;
+
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].join();
+            soma += threads[i].getSoma();
+        }
+
+        tempo = System.currentTimeMillis() - inicio;
+
+        System.out.println("\nSoma com 10 Threads: " + soma);
+        System.out.println("Tempo: " + tempo + " ms");
+
+        inicio = System.currentTimeMillis();
+
+        numThreads = 100;
+        threads = new SomaThread[numThreads];
+        faixa = numeros.length / numThreads;
+
+        inicioFaixa = 0;
+
+        for (int i = 0; i < numThreads; i++) {
+
+            int fimFaixa = inicioFaixa + faixa;
+
+            threads[i] = new SomaThread(numeros, inicioFaixa, fimFaixa);
+            threads[i].setName("Thread-" + i);
+            threads[i].start();
+
+            inicioFaixa = fimFaixa;
+        }
+
+        soma = 0;
+
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].join();
+            soma += threads[i].getSoma();
+        }
+
+        tempo = System.currentTimeMillis() - inicio;
+
+        System.out.println("\nSoma com 100 Threads: " + soma);
+        System.out.println("Tempo: " + tempo + " ms");
+    }
+}
---
+++ Soma.java
@@ -0,0 +1,24 @@
+class SomaThread extends Thread {
+
+    private short[] numeros;
+    private int inicio;
+    private int fim;
+    private long soma = 0;
+
+    public SomaThread(short[] numeros, int inicio, int fim) {
+        this.numeros = numeros;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public long getSoma() {
+        return soma;
+    }
+
+    @Override
+    public void run() {
+        for (int i = inicio; i < fim; i++) {
+            soma += numeros[i];
+        }
+    }
+}
