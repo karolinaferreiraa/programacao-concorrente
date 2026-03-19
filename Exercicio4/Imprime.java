dd3205bfdbd7c28e3dd0664776171c2cc1706bbf
Karolina Ferreira <karolinaferreira518@gmail.com>
2026-03-19 14:16:55 -0300

    Correção Exercicio4

---
+++ Exercicio4/Imprime.java
@@ -0,0 +1,25 @@
+package Exercicio4;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Imprime extends Thread {
+    private List<Integer> numeros;
+
+    public Imprime(List<Integer> numeros) {
+        this.numeros = numeros;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (numeros) {
+                System.out.println(numeros);
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+            }
+        }
+    }
+}
